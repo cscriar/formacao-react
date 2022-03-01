e550a58da2e86eab73cb20965a3a88dfda597d17
cscriar <cscriar@gmail.com>
2022-03-01 01:55:45 -0300

    Final modulo componentes

--- src/components/ListaDeCategorias/ListaDeCategorias.jsx
+++ src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -1,22 +1,43 @@
 import React, { Component } from 'react';
+import "./estilo.css";
 
 class ListaDeCategorias extends Component {
 
+
+   constructor() {
+      super();
+      this.state = {categorias:[]}
+      this._novasCategorias = this._novasCategorias.bind(this);
+   }
+
+   componentDidMount() {
+      this.props.categorias.inscrever(this._novasCategorias);
+   }
+
+   componentWillUnmount() {
+      this.props.categorias.desinscrever(this._novasCategorias);
+   }
+
+   _novasCategorias(categorias) {
+      this.setState({...this.state, categorias})
+   }
+
    _handleEventoInput (e) {
       if(e.key == "Enter") {
-         console.log("adicionar categoria")
+         let valorCategoria = e.target.value;
+         this.props.adicionarCategoria(valorCategoria);
       }
    }
     
     render() { 
         return ( 
-            <section>
-                <ul>
-                    <li> Categoria 3</li>
-                    <li> Categoria 2 </li>
-                    <li> Categoria 1 </li>
+            <section className="lista-categorias">
+                <ul className="lista-categorias_lista">
+                   {this.props.categorias.categorias.map((categoria, index) => {
+                     return <li key={index} className="lista-categorias_item">{categoria}</li>
+                   })}
                 </ul>
-                <input type="text" className="lista-categorias-input" placeholder="Adiciona uma categoria" onKeyUp={this._handleEventoInput.bind(this)}/>
+                <input type="text" className="lista-categorias_input" placeholder="Adiciona uma categoria" onKeyUp={this._handleEventoInput.bind(this)}/>
             </section>   
         );
     }
